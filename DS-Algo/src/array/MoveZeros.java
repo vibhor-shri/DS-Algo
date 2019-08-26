bd1eb24156d09c353022f8dfbc91d1faadd51a2e
vibhorshrivastava <vibhorshrivastava@192.168.1.107>
2019-08-27 03:15:00 +0530

    Move zeros to the end of the array

---
+++ DS-Algo/src/array/MoveZeros.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.Arrays;
+
+/*
+ * Given an array of integers, move all 0's to the end of it,
+ *  while maintaining the relative order of the non-zero elements.
+ */
+
+public class MoveZeros {
+
+	public static void main(String[] args) {
+//		int[] array = { 0, 1, 0, 3, 12 };
+//		int[] array = {1, 2, 0, 4, 3, 0, 5, 0};
+		int[] array = {1, 2, 0, 0, 0, 3, 6};
+//		int[] array = {1, 2, 5, 7, 9, 3, 6};
+		System.out.println(Arrays.toString(moveZeros(array)));
+	}
+
+	private static int[] moveZeros(int[] array) {
+		if (array == null) {
+			return null;
+		}
+
+		int index = 0;
+		int length = array.length;
+		for (int i = 0; i < length; i++) {
+			if (array[i] != 0) {
+				array[index++] = array[i];
+			}
+		}
+		if (index < length - 1) {
+			for (int i = index; i < length; i++) {
+				array[i] = 0;
+			}
+		}
+		return array;
+	}
+}
